b95ee903330c567c2ee505058889837988f3102f
c3b5aw <dev@c3b5aw.dev>
2022-07-11 18:36:17 +0200

    feat: add error message

--- multiauth/types/errors.py
+++ multiauth/types/errors.py
@@ -5,7 +5,11 @@ class AuthenticationError(Exception):
 
     """Specific error class raised when the authentication fails."""
 
+    message = 'authentication error'
+
 
 class ExpiredTokenError(Exception):
 
     """Specific error classed raised when the token is expired."""
+
+    message = 'token is expired'
